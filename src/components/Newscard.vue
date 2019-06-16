1b9b6b87e000baa1937fdd7c32fcdcfe27fc99af
Yannic Brügger <yannic.bruegger@gmail.com>
2019-06-16 11:25:24 +0200

    Add alternative news design

--- src/components/Newscard.vue
+++ src/components/Newscard.vue
@@ -1,26 +1,14 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="1000"
-  >
-  
+  <v-card class="mx-auto" max-width="1000">
     <v-card-title>
-      <v-icon
-        large
-        left
-      >
-        
-      </v-icon>
-      <span class="title font-weight-light"> {{ modul }}
-         <br>
-      <font size="-2"> {{ datum }} </font>
+      <span class="title font-weight-light">
+        {{ modul }}
+        <br>
+        <font size="-2">{{ datum }}</font>
       </span>
     </v-card-title>
 
-    <v-card-text style="text-align:start">
-        {{ inhalt }}
-     
-    </v-card-text>
+    <v-card-text style="text-align:start">{{ inhalt }}</v-card-text>
 
     <v-card-actions>
       <v-list-tile class="grow">
@@ -30,41 +18,29 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
           ></v-img>
         </v-list-tile-avatar>
-
         <v-list-tile-content>
-          <v-list-tile-title> {{ autor }} </v-list-tile-title>
+          <v-list-tile-title>{{ autor }}</v-list-tile-title>
         </v-list-tile-content>
-
-        <v-layout
-          align-center
-          justify-end
-        >
-        
-        </v-layout>
       </v-list-tile>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-  export default {
-
-    data: () => ({
-      //
-    }),
-    props : {
-      inhalt: String,
-      datum: String,
-      autor: String,
-      modul: String
-    }
-  
-   
+export default {
+  data: () => ({
+    //
+  }),
+  props: {
+    inhalt: String,
+    datum: String,
+    autor: String,
+    modul: String
   }
+};
 </script>
 content_copy
 
 
 <style>
-
 </style>
---
+++ src/components/TimeLinePost.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-timeline-item large>
+    <template v-slot:icon>
+      <v-avatar>
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">
+      </v-avatar>
+    </template>
+    <template v-slot:opposite>
+      <span>{{autor}}</span>
+    </template>
+    <v-card class="mx-auto" max-width="1000">
+      <v-card-title>
+        <span class="title font-weight-light">
+          {{ modul }}
+          <br>
+          <font size="-2">{{ autor }} {{ datum }}</font>
+        </span>
+      </v-card-title>
+      <v-card-text style="text-align:start">{{ inhalt }}</v-card-text>
+    </v-card>
+  </v-timeline-item>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    //
+  }),
+  props: {
+    inhalt: String,
+    datum: String,
+    autor: String,
+    modul: String
+  }
+};
+</script>
+
+
+<style>
+</style>
+
+
+
